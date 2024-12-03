3a0de96d3a9611d8ee0ab66cf5889308e7a4ed4c
Imratuzzahan Sumi <imratuzzahansumi@gmail.com>
2024-12-03 07:34:59 +0600

    Some basic function explore like split, replace, upper,lower etc

--- Basic/hello.py
+++ Basic/hello.py
@@ -2,4 +2,33 @@ print("Hello Word")
 x = 56
 y = 3.8
 print(round(x/y))
-# concatenate only string, so we need typecast to use int,, int,double, float convert to string is must for concatenating.
+name,age = ("Sumi",24)
+# concatenate only string, so we need typecast to use int, int,double, float convert to string is must for concatenating.
+print("Hello I'm "+ name + " I'm "+str(age))
+# String formating
+#argument position
+print('Hello I am  {name} and  I am {age}'.format(name=name, age=age))
+# f- string
+print(f'Hello I am  {name} and  I am {age}')
+s = 'hello world'
+print("Capitalize "+s.capitalize())
+print('Upper '+s.upper())
+print('Lower ' +s.lower())
+print('Swap case ' + s.swapcase())
+print('Length s: ')
+print(len(s))
+print('Replace: ' + s.replace('world','everyone'))
+print('Count o: ')
+print(s.count('o'))
+print(s.startswith('h'))
+print(s.endswith('d'))
+print(s.split())
+print('find r: ')
+print( s.find('r'))
+# space is not acceptable for alphanumeric or alphabetic
+print('Is all alphanumeric? ')
+print(s.isalnum())
+print('Is all alphabetic? ')
+print(s.isalpha())
+print('Is all alphanumeric? ')
+print(s.isnumeric())
