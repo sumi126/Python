e1cd948d408ec27c7d991df6d4e58187bd515f18
Imratuzzahan Sumi <imratuzzahansumi@gmail.com>
2025-04-25 08:34:58 +0600

    Let's play the ' guess the number' game

---
+++ Basic/guess-the-number.py
@@ -0,0 +1,24 @@
+import random
+p1 = input("Enter the name of player1: ")
+p2 = input("Enter the name of player2: ")
+guess_number = random.randint(1,100)
+count = 0
+while True:
+    count +=1
+    try:
+        if count % 2 == 0:
+            guess = int(input(f"{p2} enter your guess number: "))
+        else:
+           guess = int(input(f"{p1} enter your guess number: "))
+        if guess > guess_number:
+           print('Too High')
+        elif guess < guess_number:
+           print('Too Low')
+        else:
+           if count % 2 == 0:
+                print(f'Congratulations {p2}! You are the winner.  ')
+           else:
+              print(f'Congratulations {p1}! You are the winner.  ')
+           break
+    except ValueError:
+     print('Please enter valid number.')
